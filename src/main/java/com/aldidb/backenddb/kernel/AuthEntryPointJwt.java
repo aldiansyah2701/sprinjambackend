51440ece47dc03ce5ef225d72180a517ddcbe9d7
aldiansyah <aldiansyah.ramadlan@asyx.com>
2022-01-31 15:25:19 +0700

    add exeption handler AuthenticationEntryPoint

--- src/main/java/com/aldidb/backenddb/kernel/AuthEntryPointJwt.java
+++ src/main/java/com/aldidb/backenddb/kernel/AuthEntryPointJwt.java
@@ -6,18 +6,54 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.aldidb.backenddb.message.BaseResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ControllerAdvice
 @Component
-public class AuthEntryPointJwt implements AuthenticationEntryPoint  {
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		System.out.println(authException.getMessage());
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: " + authException.getMessage());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		BaseResponse baseResponse = new BaseResponse();
+		baseResponse.setMessage(authException.getMessage());
+		response.getWriter().write(convertObjectToJson(baseResponse));
+	}
+
+	@ExceptionHandler(value = { AccessDeniedException.class })
+	public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			AccessDeniedException accessDeniedException) throws IOException {
+		httpServletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+		BaseResponse baseResponse = new BaseResponse();
+		baseResponse.setMessage(accessDeniedException.getMessage());
+		httpServletResponse.getWriter().write(convertObjectToJson(baseResponse));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public void handleException(HttpServletResponse res) throws IOException {
+		BaseResponse baseResponse = new BaseResponse();
+		System.out.println(convertObjectToJson(res));
+		res.sendError(HttpStatus.BAD_REQUEST.value(), "Something went wrong");
+	}
+
+	public String convertObjectToJson(Object object) throws JsonProcessingException {
+		if (object == null) {
+			return null;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(object);
 	}
 
 }
